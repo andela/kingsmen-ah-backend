3b5c3c1b42d53c9daf23e8add062d0844efb61d2
GerrardE <ezeugwagerrard@gmail.com>
2019-06-02 21:39:52 +0100

    mend

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from '../../config/swagger';
 import userRoutes from './users';
 
 const apiRoutes = express.Router();
 
 apiRoutes.use('/', userRoutes);
-
-// swagger-ui-express for API endpoint documentation
-apiRoutes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default apiRoutes;
