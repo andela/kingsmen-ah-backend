7509ebd4fbdb0efaee694a5c429e7627e107ba06
GerrardE <ezeugwagerrard@gmail.com>
2019-06-06 12:17:53 +0100

    chore-[166272330]: Setup Swagger API docs
    - Modify swagger js/yaml files
    
    [Finishes #166272330]

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -4,6 +4,9 @@ import { config } from 'dotenv';
 // Initialize dotenv
 config();
 
+// define host url
+const host = process.env.HOST_NAME || 'localhost:3000';
+
 // Swagger Definitions
 const swaggerDefinition = {
   info: {
@@ -11,7 +14,7 @@ const swaggerDefinition = {
     version: '1.0.0',
     description: 'A Social platform for the creative at heart',
   },
-  host: `${process.env.HOST_NAME}` || 'localhost:3000',
+  host,
   basePath: '/api'
 };
 
