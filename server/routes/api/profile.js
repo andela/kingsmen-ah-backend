397b4fcbae20d924a78b3dc9a0ce462841a2f055
Olatunbosun Adeoye <adex001@gmail.com>
2019-06-16 10:38:30 +0100

    feat-[166272346]: Users follow each other move the follow and unfollow controller from user controller to profile controller update the follow and unfollow method to return profile of who followed

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import Token from '@helpers/Token';
-import UserController from '@controllers/users';
 import ProfileController from '@controllers/profile';
 
 const profileRoute = Router();
 
-profileRoute.post('/:username/follow', Token.authorize, UserController.follow);
-profileRoute.delete('/:username/follow', Token.authorize, UserController.unfollow);
+profileRoute.post('/:username/follow', Token.authorize, ProfileController.follow);
+profileRoute.delete('/:username/follow', Token.authorize, ProfileController.unfollow);
 profileRoute.get('/:username', ProfileController.getProfile);
 
 export default profileRoute;
