9d09dec5a7d159a81972e982a99b3a585e0e3899
GerrardE <ezeugwagerrard@gmail.com>
2019-06-17 20:53:02 +0100

    ft-[166272344]: Implement Comment Functionality
    - Write more tests
    
    [Delivers #166272344]

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,5 +1,8 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+import CommentController from '../controllers/comment';
+import validateComment from '../validations/comment';
 import app from '../app';
 
 chai.use(chaiHttp);
@@ -33,5 +36,49 @@ describe('TEST TO CHECK ROUTE ', () => {
     } catch (err) {
       throw err.message;
     }
+  });
+});
+
+describe('CommentController', () => {
+  let sandbox = null;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should handle error on creation of a new comment', async () => {
+    const stubFunc = { validateComment };
+
+    sandbox.stub(stubFunc, 'validateComment').rejects('aPi');
+    const next = sinon.spy();
+
+    await CommentController.create({}, {}, next);
+    sinon.assert.calledOnce(next);
+  });
+
+  it('should handle error on getting comments', async () => {
+    const next = sinon.spy();
+
+    await CommentController.getComments({}, {}, next);
+    sinon.assert.calledOnce(next);
+  });
+
+  it('should handle error on updating a comment', async () => {
+    const stubFunc = { validateComment };
+
+    sandbox.stub(stubFunc, 'validateComment').rejects('aPi');
+    const next = sinon.spy();
+
+    await CommentController.updateComment({}, {}, next);
+    sinon.assert.calledOnce(next);
+  });
+
+  it('should handle error on `failed` delete of a comment', async () => {
+    const next = sinon.spy();
+
+    await CommentController.deleteComment({}, {}, next);
+    sinon.assert.calledOnce(next);
   });
 });
