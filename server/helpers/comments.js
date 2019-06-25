4ee0c3b2f00b1ff764833648772243a223b4d477
GerrardE <ezeugwagerrard@gmail.com>
2019-06-25 10:10:37 +0100

    feat-[166272357]: Implement Comment Like Functionality
    - Refactor comment helper function
    
    [Delivers #166272357]

--- server/helpers/comments.js
+++ server/helpers/comments.js
@@ -3,54 +3,36 @@ import Sequelize from 'sequelize';
 
 const { Comment } = models;
 
-export const comments = async (value) => {
-  const payload = await Comment.findAll({
-    attributes: ['id', 'createdAt', 'updatedAt', 'body',
-      [Sequelize.fn('COUNT', Sequelize.col('CommentLikes.userId')), 'likeCount']],
-    where: {
-      articleId: value
-    },
-    order: ['createdAt'],
+const commentObt = value => ({
+  attributes: ['id', 'createdAt', 'updatedAt', 'body',
+    [Sequelize.fn('COUNT', Sequelize.col('CommentLikes.userId')), 'likeCount']],
+  where: value,
+  order: ['createdAt'],
+  include: [{
+    model: models.User,
+    as: 'author',
+    attributes: ['username'],
     include: [{
-      model: models.User,
-      as: 'author',
-      attributes: ['username'],
-      include: [{
-        model: models.Profile,
-        as: 'profile',
-        attributes: ['bio', 'avatar']
-      }]
-    },
-    {
-      model: models.CommentLike,
-      attributes: []
-    }],
-    group: ['Comment.id', 'author.id', 'author->profile.id']
-  });
+      model: models.Profile,
+      as: 'profile',
+      attributes: ['bio', 'avatar']
+    }]
+  },
+  {
+    model: models.CommentLike,
+    attributes: []
+  }],
+  group: ['Comment.id', 'author.id', 'author->profile.id']
+});
+
+export const comments = async (value) => {
+  const payload = await Comment.findAll(commentObt({ articleId: value }));
 
   return payload;
 };
 
 export const singleComment = async (id) => {
-  const payload = await Comment.findOne({
-    attributes: ['id', 'createdAt', 'updatedAt', 'body', [Sequelize.fn('COUNT', Sequelize.col('CommentLikes.userId')), 'likeCount']],
-    where: { id },
-    include: [{
-      model: models.User,
-      as: 'author',
-      attributes: ['username'],
-      include: [{
-        model: models.Profile,
-        as: 'profile',
-        attributes: ['bio', 'avatar']
-      }]
-    },
-    {
-      model: models.CommentLike,
-      attributes: [],
-    }],
-    group: ['Comment.id', 'author.id', 'author->profile.id']
-  });
+  const payload = await Comment.findOne(commentObt({ id }));
 
   return payload;
 };
