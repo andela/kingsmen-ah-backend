efc729f2150bef5ef4de9a02450b0544f215be60
Ahaiwe Emmanuel <32358797+emmygozi@users.noreply.github.com>
2019-07-30 12:09:41 +0100

    #167494732 Modify Email Template (#54)
    * chore-[167223009]: Enable Cors
    
    - Enable cors for api routes
    - modify email template
    
    [Finishes #167494732]

--- server/helpers/mailer.js
+++ server/helpers/mailer.js
@@ -7,15 +7,18 @@ config();
 const url = process.env.BASE_URL || 'localhost:3000';
 const projectName = process.env.PROJECT_NAME || 'Author\'s Haven';
 const projectEmail = process.env.PROJECT_EMAIL || 'authorshaven@gmail.com';
+const logo = 'https://i.imgur.com/bud9yrR.png"';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+
 // Configure the mail gen
 const mailGenerator = new Mailgen({
-  theme: 'cerberus',
+  theme: 'default',
   product: {
     name: projectName,
-    link: url
+    link: url,
+    logo
   }
 });
 
@@ -34,11 +37,12 @@ const sendVerifyMailToken = (token, email, name) => {
   const emailBody = {
     body: {
       name,
-      intro: `Welcome ${name}, glad to have you onboard. Please verify your mail to enjoy premium access.`,
+      title: `<h1 style="text-align: center; color: #000000"> ${projectName} </h1>`,
+      intro: `Welcome <b>${name.toUpperCase()}</b>, glad to have you onboard. Please verify your mail to enjoy premium access.`,
       action: {
         instructions: 'Click on the button below to verify your mail and start enjoying full access.',
         button: {
-          color: '#1a82e2',
+          color: '#335BCF',
           text: 'Verify Your Account',
           link: `${url}/auth/activate_user?email=${email}&token=${token}`
         }
@@ -62,11 +66,12 @@ const sendForgotPasswordMail = (token, email, name) => {
   const emailBody = {
     body: {
       name,
+      title: `<h1 style="text-align: center; color: #000000"> ${projectName} </h1>`,
       intro: 'You are receiving this email because a password reset request for your account was received.',
       action: {
         instructions: 'Tap the button below to reset your customer account password. If you didn\'t request a new password, you can safely delete this email.',
         button: {
-          color: '#1a82e2',
+          color: '#335BCF',
           text: 'Reset Your Password',
           link
         }
@@ -88,6 +93,7 @@ const sendResetSuccessMail = (email, name) => {
   const emailBody = {
     body: {
       name,
+      title: `<h1 style="text-align: center; color: #000000"> ${projectName} </h1>`,
       intro: 'You are receiving this email because a password reset request for your account was received.',
       action: {
         instructions: `Your password has been successfully reset. Please login to ${projectName} by clicking the button below`,
